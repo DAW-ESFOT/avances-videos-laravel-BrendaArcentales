2aeca01c1389cab2bf3e882032f7b061f3ede692
Brenda Arcentales <brenda.arcentales@epn.edu.ec>
2020-12-17 14:49:17 -0500

    #11 Modelo Categorie All about Category

--- database/seeders/UsersTableSeeder.php
+++ database/seeders/UsersTableSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Category;
 use App\Models\User;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Hash;
@@ -28,11 +29,19 @@ class UsersTableSeeder extends Seeder
             'password'=> $password,]);
         // Generar algunos usuarios para nuestra aplicacion
         for($i = 0; $i < 10; $i++) {
-            User::create([
+            $user=User::create([
                 'name'=> $faker->name,
                 'email'=> $faker->email,
                 'password'=> $password,
                 ]);
-}
+            $user->categories()->saveMany(
+                $faker->randomElements(
+                    array(
+                        Category::find(1),
+                        Category::find(2),
+                        Category::find(3)
+                    ), $faker->numberBetween(1, 3), false)
+            );
+        }
     }
 }
