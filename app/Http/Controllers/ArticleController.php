9a8866b13ec7d62b02d861e5686ec2ad4d7e7bb6
Brenda Arcentales <brenda.arcentales@epn.edu.ec>
2020-12-30 16:02:25 -0500

    #13 Validaciones de datos Articles

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -6,9 +6,19 @@ use App\Models\Article;
 use App\Http\Resources\Article as ArticleResource;
 use App\Http\Resources\ArticleCollection;
 use Illuminate\Http\Request;
+use phpDocumentor\Reflection\Types\Self_;
 
 class ArticleController extends Controller
 {
+    private static $rules=[
+        'title' => 'required|string|unique:articles|max:255',
+        'body' => 'required',
+    ];
+    private static $messages=[
+        'required'=>'El campo :attribute es obligatorio.',
+        'body.required'=>'El body no es válido.',
+    ];
+
     public function index()
     {
         return new ArticleCollection(Article::paginate(25));
@@ -17,8 +27,17 @@ class ArticleController extends Controller
         return response()->json(new ArticleResource($article),200);
     }
     public function store(Request $request){
-        $article = Article::create($request->all());
-        return response()->json($article,201);
+        $validatedData = $request->validate(self::$rules,self::$messages);
+       /* $validator = Validator::make($request->all(),[
+            'title' => 'required|string|unique:articles|max:255',
+            'body' => 'required|string']);
+        if ($validator->fails()) {
+            return response()->json([
+                'error' => 'data_validation_failed',
+                "error_list"=>$validator->errors()], 400);
+        }*/
+        $article = Article::create($validatedData);
+        return response()->json(new ArticleResource($article), 201);
     }
     public function update(Request $request, Article $article)
     {
