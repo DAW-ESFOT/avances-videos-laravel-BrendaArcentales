2d4c7cc1967f054928d67b12bb0a16c90073f11e
Brenda Arcentales <brenda.arcentales@epn.edu.ec>
2021-01-02 19:18:55 -0500

    #13 validacion 2

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -13,6 +13,7 @@ class ArticleController extends Controller
     private static $rules=[
         'title' => 'required|string|unique:articles|max:255',
         'body' => 'required',
+        'category_id'=>'required|exists:categories,id'
     ];
     private static $messages=[
         'required'=>'El campo :attribute es obligatorio.',
@@ -27,20 +28,21 @@ class ArticleController extends Controller
         return response()->json(new ArticleResource($article),200);
     }
     public function store(Request $request){
-        $validatedData = $request->validate(self::$rules,self::$messages);
-       /* $validator = Validator::make($request->all(),[
+        $request->validate([
             'title' => 'required|string|unique:articles|max:255',
-            'body' => 'required|string']);
-        if ($validator->fails()) {
-            return response()->json([
-                'error' => 'data_validation_failed',
-                "error_list"=>$validator->errors()], 400);
-        }*/
-        $article = Article::create($validatedData);
+            'body' => 'required',
+            'category_id'=>'required|exists:categories,id'
+        ], self::$messages);
+        $article = Article::create($request->all());
         return response()->json(new ArticleResource($article), 201);
     }
     public function update(Request $request, Article $article)
     {
+        $request->validate([
+            'title' => 'required|string|unique:articles,title,'.$article->id.'|max:255',
+            'body' => 'required',
+            'category_id'=>'required|exists:categories,id'
+        ], self::$messages);
         $article->update($request->all());
         return response()->json($article,200);
     }
