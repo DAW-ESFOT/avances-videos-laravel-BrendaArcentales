a82f9ea5ddfc643caf05b74340e96a443e7e5c5f
Brenda Arcentales <brenda.arcentales@epn.edu.ec>
2021-01-07 08:53:22 -0500

    Â#14 Manejo imagenes

--- routes/api.php
+++ routes/api.php
@@ -22,6 +22,7 @@ use Illuminate\Support\Facades\Route;
 Route::post('register', 'App\Http\Controllers\UserController@register');
 Route::post('login', 'App\Http\Controllers\UserController@authenticate');
 Route::get('articles', 'App\Http\Controllers\ArticleController@index');
+Route::get('articles/{article}/image', 'App\Http\Controllers\ArticleController@image');
 Route::group(['middleware' => ['jwt.verify']], function() {
     Route::get('user', 'App\Http\Controllers\UserController@getAuthenticatedUser');
     //Articles
@@ -29,6 +30,7 @@ Route::group(['middleware' => ['jwt.verify']], function() {
     Route::post('articles', 'App\Http\Controllers\ArticleController@store');
     Route::put('articles/{article}', 'App\Http\Controllers\ArticleController@update');
     Route::delete('articles/{article}', 'App\Http\Controllers\ArticleController@delete');
+
     //Comments
     Route::get('articles/{article}/comments', 'App\Http\Controllers\CommentController@index');
     Route::get('articles/{article}/comments/{comment}', 'App\Http\Controllers\CommentController@show');
